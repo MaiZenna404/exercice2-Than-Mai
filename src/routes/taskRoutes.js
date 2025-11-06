4ba9ee5ceabf92f25a6be489c08875e8ef286345
mai <mai.than.222@gmail.com>
2025-11-06 17:17:59 +0100

    added post endpoint on swagger

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -28,8 +28,69 @@ import { getTasks, addTask, deleteTask } from '../controllers/taskControllers.js
  *         title: "First Task"
  *         description: "It's my first task !"
  *         createdAt: "2023-01-01T00:00:00Z"
+ *     BadRequest:
+ *       type: object
+ *       properties:
+ *         status:
+ *           type: integer
+ *           description: The HTTP status code
+ *         message:
+ *           type: string
+ *           description: Bad request error message
+ *       example:
+ *         status: 400
+ *         message: "Bad request"
+ *     ServerError:
+ *       type: object
+ *       properties:
+ *         status:
+ *           type: integer
+ *           description: The HTTP status code
+ *         message:
+ *           type: string
+ *           description: Internal server error
+ *       example:
+ *         status: 500
+ *         message: "Internal server error"
  */
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Tasks
+ *     description: API endpoints for managing tasks
+ *
+ * /add-task:
+ *   post:
+ *     summary: Add a new task into the list
+ *     tags:
+ *       - Tasks
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Task'
+ *     responses:
+ *       '201':
+ *         description: Task added successfully to the list
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Task'
+ *       '400':
+ *         description: Bad request (e.g., missing required fields)
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/BadRequest'
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ServerError'
+ */
 const router = express.Router();
 
 router.get('/tasks-list', getTasks);
